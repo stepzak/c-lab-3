ee0ee659733990e6eecd95e554c1746971cddcc8
Zakatin Stepan < >
2025-11-26 00:19:26 +0300

    refactor caesar utf8

--- caesar.c
+++ caesar.c
@@ -11,7 +11,8 @@
 #define YA_BIG 0xD0AF
 #define A_SMALL 0xD0B0
 #define YA_SMALL 0xD18F
-
+#define PE_SMALL 0xD0BF
+#define ER_SMALL 0xD180
 
 int caesar_cyrillic_utf8(int utf8_char, int shift) {
 
@@ -23,45 +24,36 @@ int caesar_cyrillic_utf8(int utf8_char, int shift) {
         utf8_char = E_SMALL;
         shift++;
     }
-    int byte1 = (utf8_char >> 8) & 0xFF;
-    int byte2 = utf8_char & 0xFF;
-
-
-
-    if (byte1 == 0xD0) {
-        if (byte2 >= 0x90 && byte2 <= 0xAF) {
-            if (byte2 >= 0x96) {
-                shift++;
-            }
-            int pos = byte2 - 0x90;
-            int new_pos = (pos + shift) % 32;
-            return 0xD090 + new_pos;
-
+    if (A_BIG <= utf8_char && utf8_char <= YA_BIG) {
+        if (utf8_char > E_BIG) {
+            shift++;
         }
-        if (byte2 >= 0xB0 && byte2 <= 0xBF) {
-            if (byte2 >= 0xB6) {
-                shift++;
-            }
-            int pos = byte2 - 0xB0;
-            int new_pos = (pos + shift) % 32;
-            if (new_pos < 16) {
-                return 0xD0B0 + new_pos;
-            }
-            return 0xD180 + (new_pos - 16);
-
-        }
+        int pos = utf8_char - A_BIG;
+        int new_pos = (pos + shift) % 32;
+        return A_BIG + new_pos;
     }
-    else if (byte1 == 0xD1 && byte2 >= 0x80 && byte2 <= 0x8F) {
-        shift++;
-        int pos = byte2 - 0x80 + 16;
+    if (A_SMALL <= utf8_char && utf8_char <= PE_SMALL) {
+        if (utf8_char > E_SMALL) {
+            shift++;
+        }
+        int pos = utf8_char - A_SMALL;
         int new_pos = (pos + shift) % 32;
         if (new_pos < 16) {
-
-            return 0xD0B0 + new_pos;
+            return A_SMALL + new_pos;
         }
-        return 0xD180 + (new_pos - 16);
-
+        new_pos-=16;
+        return ER_SMALL + new_pos;
     }
-
+    if (ER_SMALL <= utf8_char && utf8_char <= YA_SMALL) {
+        shift++;
+        int pos = utf8_char - ER_SMALL;
+        int new_pos = (pos + shift) % 32;
+        if (new_pos < 16) {
+            return ER_SMALL + new_pos;
+        }
+        new_pos-=16;
+        return A_SMALL + new_pos;
+    }
     return utf8_char;
+
 }
